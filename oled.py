6fa8d15fcbd841c52f6ef040618c579625a19e37
Kumo Lin <GDUT7890@outlook.com>
2017-12-27 13:26:13 +0800

    Add files via upload

--- oled.py
+++ oled.py
@@ -1,79 +1,89 @@
 import RPi.GPIO as GPIO
 import time
 
-PinCS = 11
-PinDC = 12
-PinSDIN = 13
-PinSCLK = 15
-PinRst = 16
-OLEDCMD = 0
-OLEDDATA = 1
+class Oled(object):
+	def __init__(self, csPin, dcPin, sdinPin, sclkPin, rstPin):
+		self.csPin = csPin
+		self.dcPin = dcPin
+		self.sdinPin = sdinPin
+		self.sclkPin = sclkPin
+		self.rstPin = rstPin
+		self.oledCmd = 0
+		self.oledData = 1
+		print 'Init OK'
 
-def OLED_WR_Byte(dat, cmd):
-	if(cmd):
-		GPIO.output(PinDC,1)
-	else:
-		GPIO.output(PinDC,0)
-	GPIO.output(PinCS,0)
-	for i in range(8):
-		GPIO.output(PinSCLK,0)
-		if (dat & 0x80):
-			GPIO.output(PinSDIN,1)
+	def writeByte(self, dat, cmd):
+		if(cmd):
+			GPIO.output(self.dcPin, 1)
 		else:
-			GPIO.output(PinSDIN,0)
-		GPIO.output(PinSCLK,1)
-		dat = dat << 1
-	GPIO.output(PinCS,1)
-	GPIO.output(PinDC,1)
+			GPIO.output(self.dcPin, 0)
+		GPIO.output(self.csPin, 0)
+		for i in range(8):
+			GPIO.output(self.sclkPin, 0)
+			if (dat & 0x80):
+				GPIO.output(self.sdinPin, 1)
+			else:
+				GPIO.output(self.sdinPin, 0)
+			GPIO.output(self.sclkPin, 1)
+			dat = dat << 1
+		GPIO.output(self.csPin, 1)
+		GPIO.output(self.dcPin, 1)
 
-def OLED_Set_Pos (x,y):
-	OLED_WR_Byte(0xb0+y,OLEDCMD)
-	OLED_WR_Byte(((x&0xf0)>>4)|0x10,OLEDCMD)
-	OLED_WR_Byte((x&0x0f)|0x01,OLEDCMD)
+	def setPos(self, x, y):
+		self.writeByte(0xb0+y, self.oledCmd)
+		self.writeByte(((x&0xf0)>>4)|0x10, self.oledCmd)
+		self.writeByte((x&0x0f)|0x01, self.oledCmd)
 
-def OLED_Clear():
-	for i in range(8):
-		OLED_WR_Byte(0xb0+i,OLEDCMD)
-		OLED_WR_Byte(0x00,OLEDCMD)
-		OLED_WR_Byte(0x10,OLEDCMD)
-		for j in range(128):
-			OLED_WR_Byte(0,OLEDDATA)
+	def clearAll(self):
+		for i in range(8):
+			self.writeByte(0xb0+i, self.oledCmd)
+			self.writeByte(0x00, self.oledCmd)
+			self.writeByte(0x10, self.oledCmd)
+			for j in range(128):
+				self.writeByte(0, self.oledData)
 
-def OLED_Init():
-	GPIO.output(PinRst,1)
-	time.sleep(0.1)
-	GPIO.output(PinRst,0)
-	time.sleep(0.2)
-	GPIO.output(PinRst,1)
-	OLED_WR_Byte(0xAE,OLEDCMD)
-	OLED_WR_Byte(0x00,OLEDCMD)
-	OLED_WR_Byte(0x10,OLEDCMD)
-	OLED_WR_Byte(0x40,OLEDCMD)
-	OLED_WR_Byte(0x81,OLEDCMD)
-	OLED_WR_Byte(0xCF,OLEDCMD)
-	OLED_WR_Byte(0xA1,OLEDCMD)
-	OLED_WR_Byte(0xC8,OLEDCMD)
-	OLED_WR_Byte(0xA6,OLEDCMD)
-	OLED_WR_Byte(0xA8,OLEDCMD)
-	OLED_WR_Byte(0x3f,OLEDCMD)
-	OLED_WR_Byte(0xD3,OLEDCMD)
-	OLED_WR_Byte(0x00,OLEDCMD)
-	OLED_WR_Byte(0xd5,OLEDCMD)
-	OLED_WR_Byte(0x80,OLEDCMD)
-	OLED_WR_Byte(0xD9,OLEDCMD)
-	OLED_WR_Byte(0xF1,OLEDCMD)
-	OLED_WR_Byte(0xDA,OLEDCMD)
-	OLED_WR_Byte(0x12,OLEDCMD)
-	OLED_WR_Byte(0xDB,OLEDCMD)
-	OLED_WR_Byte(0x40,OLEDCMD)
-	OLED_WR_Byte(0x20,OLEDCMD)
-	OLED_WR_Byte(0x02,OLEDCMD)
-	OLED_WR_Byte(0x8D,OLEDCMD)
-	OLED_WR_Byte(0x14,OLEDCMD)
-	OLED_WR_Byte(0xA4,OLEDCMD)
-	OLED_WR_Byte(0xA6,OLEDCMD)
-	OLED_WR_Byte(0xAF,OLEDCMD)
+	def screenInit(self):
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setwarnings(False)
+		GPIO.setup(self.csPin, GPIO.OUT)
+		GPIO.setup(self.dcPin, GPIO.OUT)
+		GPIO.setup(self.sclkPin, GPIO.OUT)
+		GPIO.setup(self.sdinPin, GPIO.OUT)
+		GPIO.setup(self.rstPin, GPIO.OUT)
+		GPIO.output(self.rstPin, 1)
+		time.sleep(0.1)
+		GPIO.output(self.rstPin, 0)
+		time.sleep(0.2)
+		GPIO.output(self.rstPin, 1)
+		self.writeByte(0xAE, self.oledCmd)
+		self.writeByte(0x00, self.oledCmd)
+		self.writeByte(0x10, self.oledCmd)
+		self.writeByte(0x40, self.oledCmd)
+		self.writeByte(0x81, self.oledCmd)
+		self.writeByte(0xCF, self.oledCmd)
+		self.writeByte(0xA1, self.oledCmd)
+		self.writeByte(0xC8, self.oledCmd)
+		self.writeByte(0xA6, self.oledCmd)
+		self.writeByte(0xA8, self.oledCmd)
+		self.writeByte(0x3f, self.oledCmd)
+		self.writeByte(0xD3, self.oledCmd)
+		self.writeByte(0x00, self.oledCmd)
+		self.writeByte(0xd5, self.oledCmd)
+		self.writeByte(0x80, self.oledCmd)
+		self.writeByte(0xD9, self.oledCmd)
+		self.writeByte(0xF1, self.oledCmd)
+		self.writeByte(0xDA, self.oledCmd)
+		self.writeByte(0x12, self.oledCmd)
+		self.writeByte(0xDB, self.oledCmd)
+		self.writeByte(0x40, self.oledCmd)
+		self.writeByte(0x20, self.oledCmd)
+		self.writeByte(0x02, self.oledCmd)
+		self.writeByte(0x8D, self.oledCmd)
+		self.writeByte(0x14, self.oledCmd)
+		self.writeByte(0xA4, self.oledCmd)
+		self.writeByte(0xA6, self.oledCmd)
+		self.writeByte(0xAF, self.oledCmd)
 
-	OLED_WR_Byte(0xAF,OLEDCMD)
-	OLED_Clear
-	OLED_Set_Pos(0,0)
+		self.writeByte(0xAF, self.oledCmd)
+		self.clearAll()
+		self.setPos(0, 0)
