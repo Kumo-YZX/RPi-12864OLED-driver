223a037ae7effdb098bfd17462632687978d37db
Kumo Lam <16816538+Kumo-YZX@users.noreply.github.com>
2018-04-21 20:35:16 +0800

    add ASCII char display module

--- display.py
+++ display.py
@@ -1,16 +1,23 @@
-import RPi.GPIO as GPIO
 from oled import Oled
 import time
+import sys
 
 csPin = 11
 dcPin = 12
 sdinPin = 13
 sclkPin = 15
 rstPin = 16
-chnWord =u'\u5317\u4eac'
-if __name__ == "__main__":
+
+def display():
     myScreen = Oled(csPin, dcPin, sdinPin, sclkPin, rstPin)
     myScreen.screenInit()
     myScreen.clearAll()
-    myScreen.displayImage()
-    myScreen.displayChn(chnStr =chnWord, pos=[0,0], fontFile='fonts/HZK32', fontSize=32)
+    if len(sys.argv)>1 and str.upper(sys.argv[1]) =='IMAGE':
+        myScreen.displayImage(fileName=sys.argv[2] if len(sys.argv)>2 else 'git.png')
+    elif len(sys.argv)>1 and str.upper(sys.argv[1]) =='CHN':
+        myScreen.displayChn(chnStr =sys.argv[2].decode('utf8') if len(sys.argv)>2 else u'\u6837\u672C')
+    else:
+        myScreen.displayAsc(ascStr=sys.argv[1] if len(sys.argv)>1 else 'example')
+
+if __name__ == "__main__":
+    display()
--- oled.py
+++ oled.py
@@ -63,29 +63,69 @@ class Oled(object):
         self.clearAll()
         self.setPos(0, 0)
 
-    def displayAsc(self, ascStr='example', pos=[0,0], fontFile='fonts/ASC16', fontSize=16):
-        pass
+    def displayAsc(self, ascStr='example', pos=[0,0], fontPrefix='fonts/ASC', fontSize=16):
 
-    def displayChn(self, chnStr=u'\u5317\u4eac', pos=[0,0], fontFile='fonts/HZK16', fontSize=16):
+        with open(fontPrefix +str(fontSize), 'rb') as fi:
+            fontList =fi.read()
+        
+        # modelList =[]
+        for charNum in range(len(ascStr)):
+            fontAddress =ord(ascStr[charNum])*fontSize
+            for block in range(fontSize/8):
+                blockModel =[]
+                # generate model of a block
+                for line in range(8):
+                    lineModel =0
+                    for part in range(fontSize/16):
+                        lineModel =lineModel +ord(fontList[fontAddress +part +line*fontSize/16 +block*fontSize/2])*(2**(8*(fontSize/16-1-part)))
+                    blockModel.append(lineModel)
+
+                # display a block
+                for column in range(fontSize/2):
+                    colValue =0
+                    self.setPos(pos[0]+fontSize/2-1-column, pos[1]+block)
+                    for line in range(8):
+                        colValue =colValue +(blockModel[line]%2)*(2**line)
+                        blockModel[line] =blockModel[line]/2
+                    self.writeByte(colValue,1)
+
+            #reach the end of this line
+            if pos[0] and not(pos[0]%(128-fontSize/2)):
+                pos[0] =0
+                pos[1] = pos[1] +fontSize/8
+            else:
+                pos[0] =pos[0] +fontSize/2
+
+            if pos[1] >=(8-fontSize/8):
+                print 'reach the end'
+                break
+        return 1
+
+    def displayChn(self, chnStr=u'\u6837\u672C', pos=[0,0], fontPrefix='fonts/HZK', fontSize=16):
+
+        #conculate quweis for chars
         quwei =[]
         for everyChar in chnStr:
             quwei.append([ord(everyChar.encode('gbk')[0])-160, ord(everyChar.encode('gbk')[1])-160])
         print quwei
-        with open(fontFile, 'rb') as fi:
+
+        #load font file as array
+        with open(fontPrefix +str(fontSize), 'rb') as fi:
             fontList =fi.read()
         modelList =[]
         for everyChar in quwei:
             fontAddress =(94*(everyChar[0]-1)+everyChar[1]-1)*(fontSize*fontSize/8)
             fontModel =[]
             # model of every line
-            for line in range(0,fontSize*fontSize/8,fontSize/8):
+            for line in range(fontSize):
                 # model of every column part
                 lineModel =0
                 for everyPart in range(fontSize/8):
-                    lineModel =lineModel +ord(fontList[fontAddress+line+everyPart])*(2**(8*(fontSize/8-1-everyPart)))
+                    lineModel =lineModel +ord(fontList[fontAddress+line*fontSize/8+everyPart])*(2**(8*(fontSize/8-1-everyPart)))
                 fontModel.append(lineModel)
             modelList.append(fontModel)
 
+        #dispaly (8pixs as a group)
         for everyChar in modelList:
             for group in range(fontSize/8):
                 for column in range(fontSize):
